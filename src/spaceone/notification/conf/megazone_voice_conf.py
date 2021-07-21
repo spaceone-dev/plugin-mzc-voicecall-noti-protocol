491068cd8c8eba55d0e03d88863c203c46356e96
Jihyung Song <bluese@megazone.com>
2021-07-21 18:03:45 +0900

    modify tail_message

--- src/spaceone/notification/conf/megazone_voice_conf.py
+++ src/spaceone/notification/conf/megazone_voice_conf.py
@@ -6,6 +6,6 @@ MEGAZONE_VOICE_CONF = {
         'message': '알람이 발생하였습니다.',
         'country_code': '82',    # KOREA
         'closing': '완료되었습니다',
-        'tail_message': '스페이스원에 응답 메시지를 보내시려면 1번을 눌러주세요.'
+        'tail_message': '메세지를 확인하셨으면 1번을 눌러주세요.'
     }
 }
