11a90d8a44d492004ea83465c572809d693416d0
Jihyung Song <bluese@megazone.com>
2021-08-31 12:27:54 +0900

    add condition: send message only if short_message is existed.

--- src/spaceone/notification/service/notification_service.py
+++ src/spaceone/notification/service/notification_service.py
@@ -39,29 +39,32 @@ class NotificationService(BaseService):
         secret_data = params.get('secret_data', {})
         channel_data = params.get('channel_data', {})
         notification_type = params['notification_type']
-        params_message = params['message']
-        send_message = params_message.get('short_message', MEGAZONE_VOICE_CONF['default']['message'])
-
         access_key = secret_data.get('access_key')
         secret_key = secret_data.get('secret_key')
         phone_number = channel_data.get('phone_number')
         country_code = channel_data.get('country_code', MEGAZONE_VOICE_CONF['default']['country_code'])
-        kwargs = {}
 
-        if 'callbacks' in params_message:
-            subscriptions = []
+        params_message = params['message']
+        send_message = params_message.get('short_message')
 
-            send_message = f'{send_message}. {MEGAZONE_VOICE_CONF["default"]["tail_message"]}'
-            for _cb in params_message['callbacks']:
-                url = _cb.get('url')
-                endpoint = parse_endpoint(url)
-                subscriptions.append({
-                    'type': endpoint['scheme'].upper(),
-                    'uri': url
-                })
+        if send_message:
+            kwargs = {}
 
-            kwargs['subscriptions'] = subscriptions
+            if 'callbacks' in params_message:
+                subscriptions = []
 
-        noti_mgr: NotificationManager = self.locator.get_manager('NotificationManager')
-        noti_mgr.dispatch(access_key, secret_key, country_code, phone_number, send_message, **kwargs)
+                send_message = f'{send_message}. {MEGAZONE_VOICE_CONF["default"]["tail_message"]}'
+                for _cb in params_message['callbacks']:
+                    url = _cb.get('url')
+                    endpoint = parse_endpoint(url)
+                    subscriptions.append({
+                        'type': endpoint['scheme'].upper(),
+                        'uri': url
+                    })
 
+                kwargs['subscriptions'] = subscriptions
+
+            noti_mgr: NotificationManager = self.locator.get_manager('NotificationManager')
+            noti_mgr.dispatch(access_key, secret_key, country_code, phone_number, send_message, **kwargs)
+        else:
+            _LOGGER.debug("[dispatch] Skip Send message. no short_message value")
